25352a3179e41ec6dc097610383bc05fde9df9da
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-07-27 16:34:20 -0700

    Remove unused components

--- src/client/app/app.jsx
+++ src/client/app/app.jsx
@@ -1,15 +1,11 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import AppLayout from './components/AppLayout';
-import Page2 from './components/Page2';
-import Index from './components/Index';
 import { browserHistory, IndexRoute, Route, Router } from 'react-router';
 
 injectTapEventPlugin();
 
 let routes = (
   <Route path="/" component={AppLayout}>
-      
-      <Route path="page2" component={Page2}/>
   </Route>
 );
 
--- src/client/app/components/Page2.jsx
+++
@@ -1,9 +0,0 @@
-class Page2 extends React.Component {
-    render() {
-        return (
-            <h1>Page2</h1>
-        )
-    }
-}
-
-export default Page2;
