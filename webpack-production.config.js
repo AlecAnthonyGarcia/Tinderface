40751d8af479a618de5c8b1ba20d8b27509997f9
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-07-28 11:58:32 -0700

    Use production for React build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -15,6 +15,11 @@ var config = {
     filename: 'app.js'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
